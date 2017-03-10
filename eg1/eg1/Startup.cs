d601a376fae2bd177cd014660097fb20c5ac966c
Dhwani Savla <dhwanis@cybage.com>
2017-03-10 16:00:08 +0530

    eg1 proj added to master

---
+++ eg1/eg1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(eg1.Startup))]
+namespace eg1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
